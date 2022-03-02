5358b2b8ea6f2585d3aff39256428ac1c07d3b06
moiz-sohail <moiz.sohail@educative.io>
2022-03-02 23:51:52 +0500

    init

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+const config = [
+  {
+    input: ["./src/chrome/content.ts"],
+    output: {
+      file: "dist/static/js/content.js",
+      name: "content",
+      format: "iife",
+    },
+    plugins: [nodeResolve(), typescript()],
+  },
+  {
+    input: ["./src/chrome/background.ts"],
+    output: {
+      file: "dist/static/js/background.js",
+      name: "content",
+      format: "iife",
+    },
+    plugins: [typescript()],
+  },
+];
+export default config;
